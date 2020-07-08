4272f835b10788a220a5c0a4e17137bd7beb1df7
Edvins Antonovs <ummahusla@gmail.com>
2020-07-08 13:29:25 +0100

    Bio component refactoring

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,55 +1,56 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { StaticQuery, graphql, Link } from 'gatsby';
 import Image from 'gatsby-image';
 
 import styles from './index.module.scss';
 
-function Bio() {
-  return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        const { author } = data.site.siteMetadata;
-        const firstName = author.split(' ')[0];
+const Bio = () => (
+  <StaticQuery
+    query={bioQuery}
+    render={data => {
+      const { author } = data.site.siteMetadata;
+      const firstName = author.split(' ')[0];
 
-        return (
-          <section className={styles.wrapper}>
-            <div className={styles.container}>
-              <h1 className={styles.welcome}>Hi, I’m {firstName}</h1>
-              <p className={styles.subtitle}>
-                I’m a developer and designer with a strong entrepreneurial spirit who builds
-                products users love. I lead a front-end team by the day and do side hustle by the
-                night.
-              </p>
-              <p>
-                In the past, I’ve worked with companies like <strong>Betterware</strong>,{' '}
-                <strong>British Gymnastics</strong>, <strong>npower</strong>,{' '}
-                <strong>Purplebricks</strong>, agencies and studios like <strong>FishEye</strong>{' '}
-                and <strong>The Studio 4</strong>. I’ve been freelancing, working on the open-source
-                and side projects since I’ve started my career.
-              </p>
-              <p>
-                Today, I am helping to build the cyber risk prioritisation platform at{' '}
-                <strong>
-                  <a href="https://www.cyberowl.io/">Cyber Owl</a>
-                </strong>
-                &nbsp;as a <strong>Lead Front-End Developer</strong>.
-              </p>
-            </div>
+      return (
+        <section className={styles.wrapper}>
+          <div className={styles.container}>
+            <h1 className={styles.welcome}>Hi, I’m {firstName}</h1>
+            <p className={styles.subtitle}>
+              I’m a developer and designer with a strong entrepreneurial spirit who builds products
+              users love. I lead a front-end team by the day and do side hustle by the night.
+            </p>
+            <p>
+              In the past, I’ve worked with companies like <strong>Betterware</strong>,{' '}
+              <strong>British Gymnastics</strong>, <strong>npower</strong>,{' '}
+              <strong>Purplebricks</strong>, agencies and studios like <strong>FishEye</strong> and{' '}
+              <strong>The Studio 4</strong>. I’ve been freelancing, working on the open-source and
+              side projects since I’ve started my career.
+            </p>
+            <p>
+              Today, I am helping to build the cyber risk prioritisation platform at{' '}
+              <strong>
+                <a href="https://www.cyberowl.io/">Cyber Owl</a>
+              </strong>
+              &nbsp;as a <strong>Lead Front-End Developer</strong>.
+            </p>
+            <p>
+              You can <Link to={`/blog`}>read my blog</Link> or{' '}
+              <Link to={`/about`}>read more about me here</Link>.
+            </p>
+          </div>
 
-            <div>
-              <Image
-                fixed={data.avatar.childImageSharp.fixed}
-                alt={author}
-                className={styles.avatar}
-              />
-            </div>
-          </section>
-        );
-      }}
-    />
-  );
-}
+          <div>
+            <Image
+              fixed={data.avatar.childImageSharp.fixed}
+              alt={author}
+              className={styles.avatar}
+            />
+          </div>
+        </section>
+      );
+    }}
+  />
+);
 
 const bioQuery = graphql`
   query BioQuery {
